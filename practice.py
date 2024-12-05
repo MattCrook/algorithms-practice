97cfc7b6bc537a3e69e20884cf59422aa4e41741
Matt Crook <matt.crook11@gmail.com>
2024-12-05 14:42:50 -0600

    feat: add js two sum II challenge.

--- practice.py
+++ practice.py
@@ -1,39 +1,49 @@
-# Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
-# The order of the elements may be changed. 
-# Then return the number of elements in nums which are not equal to val.
-# Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
-# Change the array nums such that the first k elements of nums contain the elements which are not equal to val.
-# The remaining elements of nums are not important as well as the size of nums.
 
+#------------------------------------------------#
+# Two Sum II - Input Array Is Sorted
 
+# Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. 
+# Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
 
-# nums1 = [3,2,2,3]
-# val1 = 3
-nums2 = [0,1,2,2,3,0,4,2]
-numstwo = [0,1,2,2,3,0,4,2] # [0,1,3,0,4,2]
+# Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
 
-val2 = 2
+# The tests are generated such that there is exactly one solution. You may not use the same element twice.
+# Your solution must use only constant extra space.
+
+# Example 1:
+# Input: numbers = [2,7,11,15], target = 9
+# Output: [1,2]
+# Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+
+# Example 2:
+# Input: numbers = [2,3,4], target = 6
+# Output: [1,3]
+# Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+
+# Example 3:
+# Input: numbers = [-1,0], target = -1
+# Output: [1,2]
+# Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+# #------------------------------------------------#
 
 class Solution(object):
-    def removeElement1(self, nums, val):
-      for i in nums:
-        if i == val:
-          nums.pop(i)
-      return nums
-
-    def removeElement(self, nums, val):
-      i = 0
-      while i < len(nums):
-        if nums[i] == val:
-          nums.pop(i)
+  def twoSum(self, numbers, target):
+    n = len(numbers)
+    for i in range(n):
+      left = i + 1
+      right = n - 1
+      # if numbers[i] + numbers[right] == target:
+      #   return [numbers[i], numbers[right]]
+      while left < right:
+        mid = (left + right) // 2
+        if numbers[mid] + numbers[i] >= target:
+          right = mid
         else:
-          i += 1
-      return nums
+          left = mid + 1
+        if numbers[left] + numbers[i] == target:
+          return [i + 1, left + 1]
 
-
-
-
-print("original", nums2)
-s = Solution()
-print(s.removeElement1([0,1,2,2,3,0,4,2], 2)) # [0,1,3,0,4,2]
-print(s.removeElement([0,1,2,2,3,0,4,2], 2)) # [0, 1, 0, 4, 2]
+solution = Solution()
+print(solution.twoSum([2, 7, 11, 15], 9));
+print(solution.twoSum([2,3,4], 6));
+print(solution.twoSum([-1, 0], -1));
