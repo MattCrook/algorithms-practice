2e826f3d88c4baeb9f9f44a9b844bb1d5376986a
Matt Crook <matt.crook11@gmail.com>
2024-12-20 13:30:05 -0600

    fix: add comments to twoSum hashtable examples.

--- twoSum/twoSum.js
+++ twoSum/twoSum.js
@@ -1,8 +1,25 @@
+//**************************************************************//
 /*
-A naive approach to this problem would be to loop through each number
-and then loop again through the array looking for a pair that sums to "target_sum".
-The running time for the below solution would be O(n2) because in the worst case we are looping through the array twice to find a pair.
+One of the many popular algorithms is the Two Sum Algorithm.
+Given an array of numbers and a stand alone number, return all combinations of numbers in the array that add up to the stand alone number.
+Although any approach that works is technically a solve, but Big O determines which is the best answer for your application.
+
+Take the following example:
+
+let array = [2,5,8,3,-2,9,0]
+let targetSum = 3
+
+- The array of numbers we’re working with is 2,5,8,3,-2,9,0.
+- We need to find the number combinations that we’ll equal 3, our “targetSum”.
 */
+//**************************************************************//
+
+
+//******************** BRUTE FORCE ************************//
+// A naive approach to this problem would be to loop through each number
+// and then loop again through the array looking for a pair that sums to "target_sum".
+// The running time for the below solution would be O(n2) because in the worst case we are looping through the array twice to find a pair.
+//************************//
 function twoSumBruteForce(arr, target_sum) {
   var sums = [];
 
@@ -24,17 +41,23 @@ function twoSumBruteForce(arr, target_sum) {
 console.log(twoSumBruteForce([3, 5, 2, -4, 8, 11], 7));
 console.log("------------------");
 
+//************** HashTable/ HashMap **********************//
 /*
 We can write a faster algorithm that will find pairs that sum to target_sum in linear time.
 The algorithm below makes use of hash tables which have a constant lookup time.
 As we pass through each element in the array, we check to see if S minus the current element exists in the hash table.
 We only need to loop through the array once, resulting in a running time of O(n) since each lookup and insertion in a hash table is O(1).
 
-(1) The hash table is initially empty and the first element in the array is 4. We simply put 4 into the hash table.
-(2) The next element is 5. We check to see if the sum minus the current element exists in the hash table. 6 - 5 = 1 does not exist in the hash table. So add 5 to the hash table.
-(3) The next element is 1. We check to see if the sum minus the current element exists in the hash table. 6 - 1 = 5 does exist in the hash table so we found a pair!
+(1) The hash table is initially empty and the first element in the array is 4.
+  - We simply put 4 into the hash table.
+(2) The next element is 5.
+  - We check to see if the sum minus the current element exists in the hash table. 6 - 5 = 1 does not exist in the hash table. 
+  - So add 5 to the hash table.
+(3) The next element is 1.
+  - We check to see if the sum minus the current element exists in the hash table. 6 - 1 = 5 does exist in the hash table so we found a pair.
 */
-function twoSum(arr, target_sum) {
+//************************//
+function twoSumHashTable(arr, target_sum) {
   var sums = [];
   var hashTable = {};
 
@@ -57,10 +80,11 @@ function twoSum(arr, target_sum) {
   return sums;
 }
 
-console.log(twoSum([4, 5, 1, 8], 6));
+console.log(twoSumHashTable([4, 5, 1, 8], 6));
 console.log("----------------");
 
-// --------- Similarly using HASHMAP solution (and ES6 arrow function)------------//
+// ********** Another way Using HashMap/ HashTable (and ES6 arrow function) *********//
+// *******************************//
 const twoSumHashmap = (numbers, target) => {
   // Create oject to hold the pair ids
   //  - we will be checking against this object
