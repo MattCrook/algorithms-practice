f31acf589729e40da96a690ace073bab68e8be21
Matt Crook <matt.crook11@gmail.com>
2025-01-01 14:49:09 -0600

    feat: add selection sort to sorting algoritms.

---
+++ sort/selectionSort.js
@@ -0,0 +1,9 @@
+// ------------------------------- SELECTION SORT ALGORITHM --------------------------------- //
+/*
+Selection Sort is a simple comparison-based sorting algorithm that works by repeatedly finding the minimum (or maximum) element from the unsorted part of the list and swapping it with the first unsorted element. 
+It works by reducing the unsorted portion of the array step by step.
+- It uses in-place sorting, meaning the algorithm sorts the array in place without needing extra memory
+- it is non-stable meaning that it may change the relative order of equal elements
+- The algorithm always performs O(n²) comparisons, regardless of the input's order.
+*/
+// ------------------------------------------------------------------------------------------ //
