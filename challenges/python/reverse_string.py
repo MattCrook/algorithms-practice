2fd1c48f1b39ed6fc63c6006b0159e9e6c3c9bd5
Matt Crook <matt.crook11@gmail.com>
2025-01-10 12:18:27 -0600

    add reverse string python decorator example.

--- challenges/python/reverse_string.py
+++ challenges/python/reverse_string.py
@@ -1,10 +1,29 @@
 #------------------------------------------------#
+# Reverse an input of a given string.
 #------------------------------------------------#
 
-class Solution:
-    def reverseString(self, s):
-        for i in range(len(s)//2):
-            s[i], s[~i] = s[~i], s[i]
+# class Solution:
+#     def reverseString(self, s):
+#         for i in range(len(s)//2):
+#             s[i], s[~i] = s[~i], s[i]
 
-s = Solution()
-print(s.reverseString("cat"))
+# s = Solution()
+# print(s.reverseString("cat"))
+
+
+
+#------------------------------------------------#
+# Challenge #2: Use a python decorator to reverse an input of a given string.
+#------------------------------------------------#
+def reversal(sentence_func):
+    def reversed_sentence(*args, **kwargs):
+        original_sentence = sentence_func(*args, **kwargs)
+        return f"Reversed: {''.join(reversed(original_sentence))}"
+    return reversed_sentence
+
+
+@reversal
+def letterPress(sentence):
+    return sentence
+  
+print(letterPress("cat"))
